19ec59a0c6c3d18fb58163afc0518e82068b9430
Ubuntu <ubuntu@vps-bb6149b4.vps.ovh.net.novalocal>
2023-09-06 04:00:01 +0200

    Update from server

--- modules/theme-glass/modules/shared/constants.js
+++
@@ -1,7 +0,0 @@
-/** Module constants */
-export const CONSTANTS = {
-  MODULE_NAME: "theme-glass",
-  MODULE_TITLE: "Theme: Glass"
-};
-
-CONSTANTS.PATH = `modules/${CONSTANTS.MODULE_NAME}/`;
--- modules/theme-glass/modules/shared/helpers.js
+++
@@ -1,9 +0,0 @@
-import { CONSTANTS } from "./constants.js";
-
-/**
- * Preprend logged message with the module title
- * @param {string} msg Message to print
- */
-export const logger = (msg) => {
-  console.log(`${CONSTANTS.MODULE_TITLE} | ${msg}`);
-};
