ebe5009501d8091d1d2aea346ce0019c6f8c1993
Ubuntu <ubuntu@vps-bb6149b4.vps.ovh.net.novalocal>
2024-04-15 04:00:04 +0200

    Update from server

---
+++ modules/token-hud-wildcard/README.md
@@ -0,0 +1,106 @@
+# Token HUD Wildcard for Foundry VTT
+
+**NOTE:** This is a fork of [the original module's repository](https://github.com/javieros105/FVTT-TokenHUDWildcard) with fixes done by [MiniGrief](https://github.com/MiniGrief/FVTT-TokenHUDWildcard), myself and others, to allow people to install a fixed version for Foundry v10+. Kudos to both **javieros105** and **MiniGrief** for their work! 
+
+Use this manifest URL to install:  
+`https://raw.githubusercontent.com/mhilbrunner/token-hud-wildcard/master/module.json`
+
+**THIS MODULE IS IN MAINTENANCE MODE.** Consider switching to [Token Variant Art](https://foundryvtt.com/packages/token-variants) or other alternatives.
+
+## Compatibility and Known Issues
+
+- Default token settings may not work if the module **Token Mold** is also used due to a `preCreateToken` hook conflict (see discussion [here](https://github.com/javieros105/FVTT-TokenHUDWildcard/issues/33#issuecomment-1329177859))
+
+## Description
+
+This is a really simple module for Foundry VTT that adds a button to the Token HUD if the corresponding token has Randomized Wildcard Images activated and more than 1 detected image.
+
+When this HUD button is pressed, a small panel will be displayed on the right side of the token HUD with a list of buttons for each detected image from the Wildcard Images functionality. Pressing any of these will allow to easily change the image of the Token without opening the token configuration panel.
+
+I made this module because having to open the Token Configuration to change the image or making a macro for each token I needed to be able to rotate images was too much of a bother.
+
+Hopefully this module can help others!
+
+<p style="text-align: center;">
+<img src="https://raw.githubusercontent.com/mhilbrunner/token-hud-wildcard/master/images/prototype-config.png" alt="Prototype Config" width="500"/>
+<img src="https://raw.githubusercontent.com/mhilbrunner/token-hud-wildcard/master/images/settings.png" alt="HUD Button" height="150"/>
+<img src="https://raw.githubusercontent.com/mhilbrunner/token-hud-wildcard/master/images/hud-options1.png" alt="HUD Options" height="280"/>
+<img src="https://raw.githubusercontent.com/mhilbrunner/token-hud-wildcard/master/images/hud-options2.png" alt="HUD Options" height="280"/>
+</p>
+
+_The token images were done by the amazing Ross McConnell at [2-Minute Tabletop](https://2minutetabletop.com/) and are part of the Hero Tokens 3 Pack._
+
+## Usage
+
+To use this module you can choose in settings if you want to display the available token images as a list of file names or as a list of thumbnails.
+
+Configuring your Token is done via the Wildcard Images method, open the prototype token configuration and in the "Token Image Path" field choose a path with a * that represents a wild card, any image that follow the pattern of that path will be considered in the possible images of the token.
+
+Check the box "Randomize Wildcard Images", this will activate the wildcard and any time you drop a token in the canvas a random image will be chosen according to the path pattern.
+
+When you have the token in the canvas, opening it's HUD will show a new button if the Token has more than one image to choose from, and depending on the setting it will show the options as a file name or image (default setting is as images), pressing any of these will change the token image.
+
+If you don't want the image on drop to be chosen randomly, you can use the field in the "wildcard drop default" box in the prototype token configuration window. Picking a file here will make it the default image when dropping a new token, if you want it to be random leave it empty.
+
+If you're gonna use the default image field, I recommend you use an image file that follows the wildcard pattern. but it's not required and you can have a different default image. If you choose an image that doesn't follow the pattern you just won't be able to pick it again after changing it in the images panel.
+
+You can also use the image filename to set the dimensions of the token using the following example format:
+
+- wildcard images use this pattern `name*`
+- `name_height<number>_width<number>_scale<number>_.extension`, it's important that each parameter s preceded and followed by an underscore, so if you want to set up height, there has to be a `_height<number>_` somewhere in the name
+- you can choose to change `<number>` for a positive integer or floating number and which one to fill. If there are any parameter missing then the module will use the prototype token parameters to fill the ones missing, so you can use images with no parameter setting at all or just set one or two of the parameters.
+- these are valid filenames that will be picked up by the module `name.extension`, `name-different.extension`, `name-other_height2_scale1.5_.extension`
+
+## Installation
+
+To install the module, follow any of these methods:
+
+### Method 1
+
+- Start up Foundry and in the "Add-on Modules" tab click "Install Module".
+- In the "Manifest URL" field, paste the link: `https://raw.githubusercontent.com/mhilbrunner/token-hud-wildcard/master/module.json`
+- Click "Install" and wait for it to finish.
+- It should be installed and available in your games.
+
+### Method 2
+
+- Download the [.zip file](https://github.com/mhilbrunner/token-hud-wildcard/releases/latest/).
+- Extract the contents of the zip in your modules folder.
+- Restart Foundry and it should be available for your games.
+
+## Condensed Changelog
+
+- #### Features:
+    - Token image previews in HUD.
+    - Default token on drop. Allows the user to pick a token that will always be chosen when dropping actor on canvas instead of random chosen by the wildcard setting.
+    - Global setting to choose between filename and image previews in HUD.
+    - Global opacity setting in module settings for token previews in HUD.
+- #### Fixes:
+    - Fixed v10 compatibility
+    - Fixed wildcard default image feature
+    - Fixed token scale and console deprecation warning spam
+- #### Localization:
+    - Improved localization of all displayable text.
+    - Languages:
+        - English
+        - Spanish
+        - German
+- #### Known issues:
+    - Actor.getTokenImages() not working for users without file browser permissions
+
+## Acknowledgements
+
+Thanks to Atropos for making this amazing platform for roleplaying games. I've enjoyed it a ton and the possibility of expanding functionalities and adding your own is just awesome.
+
+Also thanks to all the module developers that have allowed me to enjoy this software even more and that were the basis for making my own little module.
+
+## Contributors
+
+- [@arbron](https://github.com/arbron): Making the update for compatibility with 0.8.x of Foundry and fixing errors.
+- [@SirTman](https://github.com/SirTman): Compatibility issue with apperance tab in prototype token foundry v9
+- [@cs96and](https://github.com/cs96and): Fix of default token rendering on preCreateToken hook that created duplicates of image and didn't render the default token.
+- [@MiniGrief](https://github.com/MiniGrief): Foundry v10 compatibility and bug fixes.
+
+## License
+
+The source code is licensed under GPL-3.0.
---
+++ modules/token-hud-wildcard/main.js
@@ -0,0 +1,180 @@
+const tokenHUDWildcard = {
+    registerSettings: () => {
+        game.settings.register('token-hud-wildcard', 'imageDisplay', {
+            name: game.i18n.format('THWildcard.DisplaySettingName'),
+            hint: game.i18n.format('THWildcard.DisplaySettingHint'),
+            scope: 'world',
+            config: true,
+            type: Boolean,
+            default: true
+        });
+        game.settings.register("token-hud-wildcard", "imageOpacity", {
+            name: game.i18n.format('THWildcard.OpacitySettingName'),
+            hint: game.i18n.format('THWildcard.OpacitySettingHint'),
+            scope: "world",
+            config: true,
+            range: { min: 0, max: 100, step: 1 },
+            type: Number,
+            default: 50
+        });
+        if (isNewerVersion(game.version, "11.0")) {
+            game.settings.register('token-hud-wildcard', 'animate', {
+                name: game.i18n.format('THWildcard.AnimateSettingName'),
+                hint: game.i18n.format('THWildcard.AnimateSettingHint'),
+                scope: 'world',
+                config: true,
+                type: Boolean,
+                default: true
+            });
+        }
+    }
+}
+
+const getTokenDimensions = (token, imgName) => {
+    var height;
+    var width;
+    var scale;
+    if (typeof imgName !== "undefined") {
+        height = imgName.match(/_height(.*)_/);
+        width = imgName.match(/_width(.*)_/);
+        scale = imgName.match(/_scale(.*)_/);
+    }
+
+    var prototypeData;
+    if (isNewerVersion(game.version, "11.0")) {
+        prototypeData = token.delta?.syntheticActor?.prototypeToken;
+    }
+    if (!isNewerVersion(game.version, "11.0") || typeof prototypeData === "undefined") {
+        prototypeData = token._source;
+    }
+
+    return {
+        height: height ? parseFloat(height[1]) : prototypeData.height,
+        width: width ? parseFloat(width[1]) : prototypeData.width,
+        scale: scale ? parseFloat(scale[1]) : prototypeData.texture.scaleX,
+    }
+}
+
+Hooks.on('init', () => {
+    tokenHUDWildcard.registerSettings();
+});
+
+Hooks.on('ready', () => {
+    const renderTokenConfig = 'renderTokenConfig';
+    Hooks.on(renderTokenConfig, WildcardDefault.render);
+    WildcardDefault._hookPreTokenCreate();
+});
+
+const WildcardDefault = {
+    getDefaultImg: async (token) => {
+        const flag = token.getFlag('token-hud-wildcard', 'default') || '';
+        return flag;
+    },
+    _hookPreTokenCreate() {
+        Hooks.on('preCreateToken', (parent, data, options, userId) => {
+            const defaultValue = parent?.actor?.prototypeToken?.getFlag('token-hud-wildcard', 'default');
+            if (defaultValue && parent?.actor?.prototypeToken?.randomImg) {
+                /*const dimensions = getTokenDimensions(parent, defaultValue);
+                let updateInfo = { img: defaultValue, ...dimensions };*/
+                parent.updateSource({"texture.src" : defaultValue});
+            }
+        });
+    },
+    render: async (config, html) => {
+        const defaultImg = await WildcardDefault.getDefaultImg(config.token);
+        if (config.token._id) {
+            return;
+        }
+        const imageDataTab = html.find('.tab[data-tab="appearance"],.tab[data-tab="image"]');
+        const checkBoxWildcard = imageDataTab.find('input[name="randomImg"]');
+        const configField = await renderTemplate('/modules/token-hud-wildcard/templates/configField.html', { defaultImg, available: checkBoxWildcard[0].checked });
+
+        imageDataTab.append(configField);
+        const defaultConfig = imageDataTab.find('.thwildcard-default');
+
+        defaultConfig.find('button').click(event => {
+            event.preventDefault();
+            const input = defaultConfig.find('input')[0];
+
+            const fp = new FilePicker({ current: input.value, field: input });
+            fp.browse(defaultImg);
+        });
+
+        checkBoxWildcard.click(event => {
+            if (event.target.checked) {
+                defaultConfig[0].classList.add('active');
+            } else {
+                defaultConfig[0].classList.remove('active');
+            }
+        });
+    }
+}
+
+Hooks.on('renderTokenHUD', async (hud, html, token) => {
+    const actor = game.actors.get(token.actorId);
+    const images = await actor?.getTokenImages() ?? [];
+
+    if (images.length < 2) {
+        return;
+    }
+
+    const imageDisplay = game.settings.get('token-hud-wildcard', 'imageDisplay');
+    const imageOpacity = game.settings.get('token-hud-wildcard', 'imageOpacity') / 100;
+    const imagesParsed = images.map(im => {
+        const split = im.split('/');
+        var extension = im.split('.');
+        extension = extension[extension.length - 1];
+        const img = ['jpg', 'jpeg', 'png', 'svg', 'webp'].includes(extension);
+        const vid = ['webm', 'mp4', 'm4v'].includes(extension);
+        return { route: im, name: split[split.length - 1], used: im === token.texture.src, img, vid, type: img || vid };
+    });
+
+    const wildcardDisplay = await renderTemplate('/modules/token-hud-wildcard/templates/hud.html', { imagesParsed, imageDisplay, imageOpacity });
+
+    html.find('div.right')
+        .append(wildcardDisplay)
+        .click((event) => {
+            let activeButton, clickedButton, tokenButton;
+            for (const button of html.find('div.control-icon')) {
+                if (button.classList.contains('active')) activeButton = button;
+                if (button === event.target.parentElement) clickedButton = button;
+                if (button.dataset.action === 'thwildcard-selector') tokenButton = button;
+            }
+
+            if (clickedButton === tokenButton && activeButton !== tokenButton) {
+                tokenButton.classList.add('active');
+
+                html.find('.thwildcard-selector-wrap')[0].classList.add('active');
+                const effectSelector = '[data-action="effects"]';
+                html.find(`.control-icon${effectSelector}`)[0].classList.remove('active');
+                html.find('.status-effects')[0].classList.remove('active');
+            } else {
+                tokenButton.classList.remove('active');
+
+                html.find('.thwildcard-selector-wrap')[0].classList.remove('active');
+            }
+        });
+
+    const buttons = html.find('.thwildcard-button-select');
+
+    buttons.map((button) => {
+        buttons[button].addEventListener('click', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            const controlled = canvas.tokens.controlled;
+            const index = controlled.findIndex(x => x.document._id === token._id);
+            const tokenToChange = controlled[index];
+            const updateTarget = tokenToChange.document;
+
+            const dimensions = getTokenDimensions(updateTarget, event.target.dataset.name);
+            let updateInfo = { img: event.target.dataset.name, ...dimensions };
+
+            if (isNewerVersion(game.version, "11.0")) {
+              const shouldAnimate = game.settings.get('token-hud-wildcard', 'animate');
+              updateTarget.update(updateInfo, {animate: shouldAnimate});
+            } else {
+              updateTarget.update(updateInfo);
+            }
+        });
+    });
+});
