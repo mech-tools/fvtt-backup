cf4bc98d2fce6719930431332b10c5281449d655
Ubuntu <ubuntu@vps-bb6149b4.vps.ovh.net.novalocal>
2024-07-14 04:00:04 +0200

    Update from server

--- modules/sync-token-name/scripts/start.js
+++ modules/sync-token-name/scripts/start.js
@@ -1,10 +1,31 @@
+
+
+
 Hooks.on("updateActor", (actor,changes,diff,userId) => {
 
+  let isv12=parseInt(game.version)>11; /// remove in the next version
+
+  
    if (game.user.id==userId && changes.name!==undefined){
-    actor.update({"token.name":changes.name});
+
+
+    /// changing name in the sheet
+    if (isv12){       
+      actor.update({"prototypeToken.name":changes.name});
+    } else {  /// remove in the next version
+      actor.update({"token.name":changes.name});
+    }
+    
    
+    /// if it's a sheet from a unlinked token (double-click)
     if (actor.isToken){
-      canvas.tokens.get(actor.token.id).data.update({"name":changes.name});
+      
+      if (isv12){
+        canvas.tokens.get(actor.token.id).document.update({"name":changes.name});
+      } else { /// remove in the next version
+        canvas.tokens.get(actor.token.id).update({"name":changes.name});
+      }
+      
     }
    
     
